484773747f6379d14bc767104c1a5ecc66545122
Danying Shao <danying.shao@gmail.com>
2021-08-31 14:33:03 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,37 +1,37 @@
+from datetime import date, timedelta
+from pathlib import Path
 from pyneid.neid import Neid
 
-Neid.query_datetime('l1', '2020-12-31 00:00:00/2020-12-31 23:59:59', outpath='./meta.xml')
-
-
-
-# include("password.jl")  # Needs to set user_nexsci and passwd_nexsci
-# cookie_nexsci = "./neidadmincookie.txt"
-
-
-#NeidArchive.login(userid=user_nexsci, password=passwd_nexsci, cookiepath=cookie_nexsci)
-
-
-
-query_result_file = "./meta.csv"
+# requested format of the meta file
 default_format = "csv"
-outdir = "."
 
-param = {}
-param["datalevel"] = "solarl1"
-param["object"] = "Sun"
-param["datetime"] = "2021-01-14 00:00:00/2021-01-14 23:59:59"
+# start and end dates
+start_date = date(2020,12,12)  
+end_date = date.today()
+delta_date = timedelta(days=1)
 
-Neid.query_criteria(param, default_format, outpath=query_result_file)
+while start_date <= end_date:
+    # create the data directory for start_date
+    outdir = Path(f'/gpfs/group/ebf11/default/RISE_NEID/data/input/solar_L1/{start_date}')
+    outdir.mkdir(parents=True, exist_ok=True)
+    
+    # filepath of the meta file
+    query_result_file = outdir.joinpath("meta.csv")
 
+    # query parameters
+    param = {}
+    param["datalevel"] = "solarl1" # L1 data only
+    param["object"] = "Sun"
+    param["datetime"] = f'{start_date} 00:00:00/{start_date} 23:59:59'
 
-num_lines = countlines(query_result_file) - 1
-println("# Query resulted in file with ", num_lines, " entries.")
+    # get the meta file
+    Neid.query_criteria(param, default_format, outpath=query_result_file)
 
+    # download the fits data
+    Neid.download(query_result_file, param["datalevel"], default_format, outdir)
 
-
-
-
-
-Neid.download(query_result_file, param["datalevel"], default_format, outdir, start_row=1, end_row=2)
-
-
+    # logging
+    num_lines = countlines(query_result_file) - 1
+    println('{start_date}: {num_lines} entries have been downloaded.')
+    
+    start_date += delta_date
